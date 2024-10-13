7c949b82e1167546e82b2694e63c98b578fc3b0d
Phan Nguyen Khanh Minh <phannguyenkhanhminh@gmail.com>
2024-10-13 12:57:34 +0700

    Initial Commit

---
+++ basicAboutUITableView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  basicAboutUITableView
+//
+//  Created by Phan Nguyễn Khánh Minh on 13/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
