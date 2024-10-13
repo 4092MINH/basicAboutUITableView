e1e71747583a6c46943d610c4ddb6eced0738e4e
Phan Nguyen Khanh Minh <phannguyenkhanhminh@gmail.com>
2024-10-13 16:10:29 +0700

    Tạo table view và một số nút có tính năng phát nhạc

---
+++ basicAboutUITableView/VCSoundTable.swift
@@ -0,0 +1,29 @@
+//
+//  VCSoundTable.swift
+//  basicAboutUITableView
+//
+//  Created by Phan Nguyễn Khánh Minh on 13/10/24.
+//
+
+import UIKit
+
+class VCSoundTable: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
